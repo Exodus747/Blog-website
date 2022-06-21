3e5bb45a3b25ded6dd71709d1a8ac1aabe935b75
Exodus747 <manvikjain2418@gmail.com>
2022-06-21 21:33:20 +0530

    profile section updated

--- NEW/django_project/users/models.py
+++ NEW/django_project/users/models.py
@@ -16,7 +16,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 450 or img.width > 450:
+            output_size = (450, 450)
             img.thumbnail(output_size)
             img.save(self.image.path)
